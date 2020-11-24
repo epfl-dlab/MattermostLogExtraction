b5008f608f25cd6d6fa3165c9d8d2670bcac7207
Loïc Houmard <loic.houmard@epfl.ch>
2020-11-24 11:24:51 +0100

    Transform sender receiver in readable ascii. Add mention and emoji extraction

--- mattermostExtract.py
+++ mattermostExtract.py
@@ -6,15 +6,17 @@ from csv_parser import write_csv
 from datetime import datetime
 import hashlib
 import message_processing as mp
+import base64
 
 
 def query_message_from_to(cur):
     """Generator function that queries who sent which message to whom at which date on which channel with additional informations
     about the message such as the id of the post, its parent id (if it was a reply to another post) to be able to construct a tree,
-    the extension of the file if it was sent with a file joined and the number of words/emojis/char in the message.
+    the extension of the file if it was sent with a file joined and the number of words/char in the message and a list of emojis and 
+    a set of mentions (with raw data for the moment)
     The sender and receivers are anonymised using MD5 hashing.
-    Only people who were on the channel at the time the message was sent are taken in account as receivers, using the channelmemberhistory table
-    from the database. 
+    Only people who were on the channel at the time the message was sent are taken in account as receivers, using the channelmemberhistory
+    table from the database. 
     Only other people are considered as receivers, not the one sending the message.
 
     Parameters
@@ -23,13 +25,14 @@ def query_message_from_to(cur):
 
     Returns
     -------
-    Generator((string, string, int, int, int string, char, list<string>, datetime, string, string, string))
+    Generator((string, string, int, int, List<String>, Set<String>, string, char, list<string>, datetime, string, string, string))
         A generator of tuples containing 
         - A string for the md5 hash of the mail of the sender
         - A string for the message
         - An int for the number of words in the message (emojis are not counted as words)
-        - An int fot the number of emojis in the message
         - An int for the number of chars in the message
+        - An list of string with every emojis in the message
+        - A set of strings with all the mentions (as raw text for now on)
         - A string for the name of the channel on which the message was sent
         - A char for the type of channel on wichh the message was sent (O for public, P for private or D for direct messages)
         - A list of string for the md5 hash of the mail of the receivers (in the order in which they joined the channel)
@@ -55,23 +58,23 @@ def query_message_from_to(cur):
     cur.execute(query)
     rows = cur.fetchall()
 
-    row_definition = ("Sender", "Message", "NumberWords", "NumberSmileys", "NumberChars", "Channel", "ChannelType", "Receivers", "Time", "PostId", "PostParentId", "FileExtension")
+    row_definition = ("Sender", "Message", "NumberWords", "NumberChars","Emojis", "Mentions", "Channel", "ChannelType", "Receivers", "Time", "PostId", "PostParentId", "FileExtension")
     yield row_definition
 
-    #Here we just do a "groupby" to have a list of receivers for the same message and we hash it with md5. We store it in a dictionnary for simplicity.
+    #Here we just do a "groupby" to have a list of receivers for the same message and we hash them with md5. We store it in a dictionnary for simplicity.
     message_to_list_receivers = OrderedDict()
     for sender, message, channel, channel_type, receiver, unix_time, post_id, post_parent_id, file_extension in rows:
         hash_receivers = message_to_list_receivers.setdefault((sender, message, channel, channel_type, unix_time, post_id, post_parent_id, file_extension), [])
         if(sender!=receiver):
-            hash_receivers.append(hashlib.md5(receiver.encode()).digest())
+            hash_receivers.append(hashlib.md5(receiver.encode()).hexdigest())
 
     
     for (sender, message, channel, channel_type, unix_time, post_id, post_parent_id, file_extension), hash_receivers in message_to_list_receivers.iteritems():
-        md5_sender = repr(hashlib.md5(sender.encode()).digest())
+        md5_sender = hashlib.md5(sender.encode()).hexdigest()
         date = datetime.fromtimestamp(unix_time/1000)
-        no_words, no_smiley = mp.count_number_words_and_smiley(message)
+        no_words, emojis, mentions = mp.count_words_emojis_mentions(message)
         no_char = len(message)
-        yield md5_sender, message, no_words, no_smiley, no_char, channel, channel_type, hash_receivers, date, post_id, post_parent_id, file_extension
+        yield md5_sender, message, no_words, no_char, emojis, mentions, channel, channel_type, hash_receivers, date, post_id, post_parent_id, file_extension
 
 
 def query_message_from_toNumber(cur):
@@ -90,13 +93,14 @@ def query_message_from_toNumber(cur):
 
     Returns
     -------
-    List((string, string, int, int, int string, char, int, datetime, string, string, string))
+    List((string, string, int, int, list<string>, set<string>, string, char, int, datetime, string, string, string))
         A list of tuples containing 
         - A string for the md5 hash of the mail of the sender
         - A string for the message
         - An int for the number of words in the message (emojis are not counted as words)
-        - An int fot the number of emojis in the message
         - An int for the number of chars in the message
+        - An list of string with every emojis in the message
+        - A set of strings with all the mentions (as raw text for now on)
         - A string for the name of the channel on which the message was sent
         - A char for the type of channel on wichh the message was sent (O for public, P for private or D for direct messages)
         - An int for the number of receivers.
@@ -122,15 +126,15 @@ def query_message_from_toNumber(cur):
     rows = cur.fetchall()
     
     def process((sender, message, channel, channel_type, number_receivers, unix_time, post_id, post_parent_id, file_extension)):
-        md5_sender = repr(hashlib.md5(sender.encode()).digest())
+        md5_sender = hashlib.md5(sender.encode()).hexdigest()
         date = datetime.fromtimestamp(unix_time/1000)
-        no_words, no_smiley = mp.count_number_words_and_smiley(message)
+        no_words, emojis, mentions = mp.count_words_emojis_mentions(message)
         no_char = len(message)
-        return md5_sender, message, no_words, no_smiley, no_char, channel, channel_type, number_receivers, date, post_id, post_parent_id, file_extension
+        return md5_sender, message, no_words, no_char, emojis, mentions, channel, channel_type, number_receivers, date, post_id, post_parent_id, file_extension
 
     result = map(process, rows)
 
-    row_definition = ("Sender", "Message", "NumberWords", "NumberSmileys", "NumberChars", "Channel", "ChannelType", "NumberReceivers", "Time", "PostId", "PostParentId", "FileExtension")
+    row_definition = ("Sender", "Message", "NumberWords", "NumberChars","Emojis", "Mentions", "Channel", "ChannelType", "NumberReceivers", "Time", "PostId", "PostParentId", "FileExtension")
     result.insert(0, row_definition)
 
     return result
--- message_processing.py
+++ message_processing.py
@@ -1,33 +1,50 @@
-def count_number_words_and_smiley(message):
+def count_words_emojis_mentions(message):
     
     no_words = 0
-    no_smiley = 0
+    
+    emojis = []
+    mentions = set()
 
     start_word = False
-    start_smiley = False
+    start_emoji = False
+    start_mention = False
+    mention=""
+    emoji=""
 
     for ch in message:
 
-        if((ch.isalpha() or ch.isdigit()) and not start_smiley):
+        if((ch.isalpha() or ch.isdigit()) and not start_emoji):
             start_word = True
-        elif(ch == ':'):
-            if(start_smiley):
-                start_smiley = False
-                no_smiley+=1
+        elif(ch.isspace()):
+            if(start_word):
+                start_word = False
+                no_words += 1
             else:
-                start_smiley = True
-        elif(ch.isspace() and start_word):
-            start_word = False
-            no_words += 1
-    
+                start_emoji = False
+            if(start_mention):
+                start_mention = False
+                mentions.add(mention)
+                mention = ""
+            
+            
+        if(start_emoji):
+            emoji += ch
+        if(start_mention):
+            mention += ch
+
+        if(ch == '@' and not start_emoji and not start_word):
+            start_mention = True
+        elif(ch == ':'):
+            if(start_emoji):
+                start_emoji = False
+                emojis.append(emoji[:-1])
+                emoji=""
+            else:
+                start_emoji = True
+
     if(start_word):
         no_words += 1
+    if(start_mention):
+        mentions.add(mention)
     
-    return no_words, no_smiley
-
-#print("Interesting {}".format(count_number_words_and_smiley("Interesting")))
-#print("Fisrt message on public channel n. 1 {}".format(count_number_words_and_smiley("Fisrt message on public channel n. 1")))
-#print(":flushed:  :innocent: :smiley:  {}".format(count_number_words_and_smiley(":flushed:  :innocent: :smiley: ")))
-
-
-
+    return no_words, emojis, mentions
